efaa0a5803fdd143830491e83bf5dd4659c5c5ac
ORANGETAN3422 <orangtan3422@gmail.com>
2026-07-26 19:58:08 +1000

    deploy thungs

---
+++ svelte.config.js
@@ -0,0 +1,18 @@
+import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+
+const base = process.env.BASE_PATH ?? '';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	preprocess: vitePreprocess(),
+	compilerOptions: {
+		runes: true
+	},
+	kit: {
+		adapter: adapter({ fallback: '404.html' }),
+		paths: { base }
+	}
+};
+
+export default config;
